3be675acdbba37f9ba5fbc2cab60eb59ad2bbc36
Will Weber <wtweber@gmail.com>
2026-09-24 08:30:25 -0600

    Revert "Corrected rubocop issues in password.rb as well as added comments."
    This reverts commit 05b06937a5226abcb7d3bb94cb3e1f490679e443.

--- lib/rubikey/password.rb
+++ lib/rubikey/password.rb
@@ -2,11 +2,9 @@
 
 require_relative 'cipher'
 
-# Stores website info and encrypts passwords using a master master password for later decryption.
 class Password
   attr_reader :id, :website, :username, :enc_password
 
-  # Initialization of a password object without the encrypted data
   def initialize(website, username)
     raise ArgumentError, 'Website can not be empty' if website.empty?
     raise ArgumentError, 'Username can not be empty' if username.empty?
@@ -16,18 +14,11 @@ class Password
   end
 
   def ==(other)
-    conditions = [
-      self.class == other.class,
-      website == other.website,
-      username == other.username,
-      enc_password == other.enc_password
-    ]
-    conditions.all
+    self.class == other.class && selfwebsite == other.website && self.username == other.username && self.enc_password == other.enc_password
   end
 
-  # Initilization of a password from the sql row returned from the stored db
   def self.new_from_db(row)
-    new(row[1], row[2])
+    self.new(row[1], row[2])
     @enc_password = row[3]
     @id = row[0]
     self
@@ -45,14 +36,12 @@ class Password
     @username = newusername
   end
 
-  # Encrypt and store new password
   def update_password(new_password, master_password)
     raise ArgumentError, 'Password can not be empty' if new_password.empty?
 
     @enc_password = PasswordCipher.encrypt(new_password, master_password)
   end
 
-  # Decrypt stored pasword data using master password
   def get_password(master_password)
     PasswordCipher.decrypt(@enc_password, master_password)
   end
